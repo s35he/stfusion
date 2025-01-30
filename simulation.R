9f031ada431f32fe816344f432cbce656e7e2b59
s35he <146749703+s35he@users.noreply.github.com>
2025-01-30 13:17:02 -0500

    Update simulation.R

--- simulation.R
+++ simulation.R
@@ -1,5 +1,49 @@
+library(INLA)
+library(gridExtra)
+library(viridisLite)
+library(mvGPS)
+library(raster)
+library(xtable)
+
 source("modelfit_fn.R")
 source("rspde_fn.R")
+source("prep.R")
+
+seed <- commandArgs(trailingOnly = TRUE)
+seed <- as.numeric(seed)[1]
+
+setwd("/u/s35he/algal_sim_results")
+
+zz <- file(paste0("./scenario0/output_sim_", seed, ".txt"), open = "wt")
+sink(zz ,type = "output")
+sink(zz, type = "message")
+
+load(paste0("./pre.rda"))
+lat = sat_preprocess$lat
+lon = sat_preprocess$lon
+key = sat_preprocess$key
+nlat = length(lat)
+nlon = length(lon)
+lonlat <- data.frame(lat=rep(lat, each=nlon), lon=rep(lon, nlat), area = 1:(nlon*nlat), 
+                     latfac = rep(1:nlat, each=nlon), lonfac=rep(1:nlon, nlat))
+lonlatkey <- lonlat[lonlat$area %in% key,]
+lonlatkey$idarea <- 1:length(key)
+
+lat = sort(unique(lonlatkey$lat), decreasing = T)
+lon = sort(unique(lonlatkey$lon))
+nlat = length(lat)
+nlon = length(lon)
+
+eda <- all_data[all_data$cat==1, ]
+eda <- eda[!is.na(eda$Latitude),]
+narea <- sat_preprocess$key.length
+coords <- as.matrix(eda[1:narea, c("Longitude", "Latitude")])
+
+tmp_poly <- create_polygon(lon=lon, lat=lat, b.ext = 0.3, coords = coords)
+r <- tmp_poly$r
+sr <- tmp_poly$sr
+sr_short <- tmp_poly$sr_short
+sr_list <- tmp_poly$sr_list
 
 n <- 19
 n_train <- 14
@@ -52,5 +96,5 @@ for (i in 1:length(mesh_list)){
   }
 }
 
-
+sink()
 
